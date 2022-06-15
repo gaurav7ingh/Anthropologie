9d316a325b364625f054a4042bf86975830a5b39
ashelake <ashelake88@gmail.com>
2022-06-15 12:26:46 +0530

    Sign in functionality

--- Sign.js
+++ Sign.js
@@ -8,26 +8,74 @@ function close() {
 }
 
 let arr = JSON.parse(localStorage.getItem("userData")) || [];
-document.querySelector("form").addEventListener("submit", function () {
+document.querySelector("#signinform").addEventListener("submit", function () {
   event.preventDefault();
-  let userObj = {
+  document.querySelector("#signIn_signUp>button").innerText = "";
+  let userObjsignin = {
     email: document.querySelector("#email").value,
     password: document.querySelector("#password").value,
   };
+
   let email = document.querySelector("#email").value;
-  if (checkfuncion(email) == true) {
+  let password = document.querySelector("#password").value;
+  // console.log(email, password);
+  if (checkfunction(email, password) == true) {
     alert("Sign up Successul");
-    arr.push(userObj);
-    localStorage.setItem("userData", JSON.stringify(arr));
+    document.querySelector("#signIn_signUp>button").innerText = email;
+
     close();
   } else {
-    alert("alredy registered");
+    alert("Something wrong");
   }
 });
+// console.log(arr);
+function checkfunction(email, password) {
+  let fill2 = arr.filter(function (elem) {
+    return email == elem.email && password == elem.password;
+  });
+  if (fill2.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-function checkfuncion(email) {
-  let fill = arr.filter(function (elem) {
-    return email === elem.email;
+// ------------sign Up------------------------
+
+document.querySelector("#signUp").addEventListener("click", function () {
+  event.preventDefault();
+  document.querySelector(".bg_modal_creat").style.display = "flex";
+});
+document.querySelector(".close_creat").addEventListener("click", close_creat);
+
+function close_creat() {
+  document.querySelector(".bg_modal_creat").style.display = "none";
+}
+
+let userArr = JSON.parse(localStorage.getItem("userData")) || [];
+document
+  .querySelector("#signupform")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let userObj = {
+      email: document.querySelector("#email_creat").value,
+      mobile: document.querySelector("#mobile_creat").value,
+      password: document.querySelector("#password_creat").value,
+    };
+    let mobile = document.querySelector("#mobile_creat").value;
+    if (checksignup(mobile) == true) {
+      userArr.push(userObj);
+      alert("account created succesfully");
+      localStorage.setItem("userData", JSON.stringify(userArr));
+      close_creat();
+    } else {
+      alert("Account alredy exist");
+    }
+  });
+
+function checksignup(mobile) {
+  let fill = userArr.filter(function (elem) {
+    return mobile === elem.mobile;
   });
   if (fill.length > 0) {
     return false;
